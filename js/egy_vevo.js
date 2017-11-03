e04a63a598dded6e992072ca155e24151326ee7e
czabadaygabor <czabadayg@gmail.com>
2017-11-03 11:49:30 +0100

    bla

--- js/egy_vevo.js
+++ js/egy_vevo.js
@@ -9,12 +9,11 @@ vevok_lekerdez();
 
 
 function vevok_lekerdez() {
-    console.log(cid);
     getJson("api/customers/" + cid, function (customers) {
         tableData = customers;
         fillTable(tableData);
     })
-};
+}
 
 function fillTable(data) {
     // Fejléc generálása.
@@ -30,23 +29,22 @@ function fillTable(data) {
         for (var j in data[k]) {
             if (j != "c_active" && j != "c_news") {
                 if (j == "c_id") {
-                    tr += "<td><input type='text' value='" + data[k][j] + "' id='" + k + "_" + j + " szerk' disabled ></td>";
+                    tr += "<td><input type='text' value='" + data[k][j] + "' id='" + j + "' disabled ></td>";
                     azonosito = data[k][j];
                 } else {
-                    tr += "<td><input type='text' value='" + data[k][j] + "' id='" + k + "_" + j + " szerk' class=input_vevo></td>";
+                    tr += "<td><input type='text' value='" + data[k][j] + "' id='" + j + "' class='input_vevo'></td>";
                 }
 
-                console.log(k + "_" + j);
+                console.log(j);
             } else {
                 tr += "<td><input type='checkbox'";
                 if (data[k][j] == 1) {
                     tr += " checked ";
                 }
-                tr += " id='" + k + " szerk' class=input_vevo></td>";
+                tr += " id='" + j + "' class='input_vevo'></td>";
             }
         }
-        tr += "<td><input type='button' value='OK' id='" + azonosito + " szerk' class=button_vevo></td>";
-        console.log(azonosito);
+        //tr += "<td><input type='button' value='OK' id='updater_gomb' class=button_vevo></td>";
         content += tr + "</tr>";
     }
     targetTable.querySelector("tbody").innerHTML = content;
@@ -59,3 +57,67 @@ function fillTable(data) {
             fillTable(tableData);
         });
     });*/
+
+
+/*function updater() {
+    var c_id = document.getElementById("c_id").value;
+    var c_name = document.getElementById("c_name").value;
+    var c_address = document.getElementById("c_address").value;
+    var c_city = document.getElementById("c_city").lue;
+    var o_nev = document.getElementById("o_nev").value;
+    var c_contact = document.getElementById("c_contact").value;
+    var c_active = document.getElementById("c_active").value;
+    var c_news = document.getElementById("c_news").value;
+
+    var customer = {
+        cid: c_id,
+        cname: c_name,
+        address: c_address,
+        city: c_city,
+        orszagnev: o_nev,
+        contact: c_contact,
+        active: c_actve,
+        news: c_news
+    };
+    postData('api/customers/update/' + customer[c_id], customer, function (response) {
+        console.log(response);
+    });
+
+
+}*/
+//document.getElementById("updater_gomb").addEventListener("click", anyádat);
+
+document.getElementById("updater_gomb").addEventListener("click", function () {
+    var c_id = document.getElementById("c_id").value;
+    var c_name = document.getElementById("c_name").value;
+    var c_address = document.getElementById("c_address").value;
+    var c_city = document.getElementById("c_city").lue;
+    var o_name = document.getElementById("o_name").value;
+    var c_contact = document.getElementById("c_contact").value;
+    if (document.getElementById("c_active").checked == true) {
+        var c_active = 1;
+    } else {
+        var c_active = 0;
+    }
+    if (document.getElementById("c_news").checked == true) {
+        var c_news = 1;
+    } else {
+        var c_news = 0;
+    }
+
+    var customer = {
+        cid: c_id,
+        cname: c_name,
+        address: c_address,
+        city: c_city,
+        orszagnev: o_name,
+        contact: c_contact,
+        active: c_actve,
+        news: c_news
+    };
+    postData('api/customers/update/' + customer[c_id], customer, function (response) {
+        console.log(response);
+    });
+
+
+});
