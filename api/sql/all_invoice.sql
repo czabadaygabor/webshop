6accc2c0c3d2e5a6c8c7310c0ac1ca7af8fdb33e
Zsofia Kubik <edhiril@gmail.com>
2017-11-03 12:53:46 +0100

    feat: invoices, dict

--- api/sql/all_invoice.sql
+++ api/sql/all_invoice.sql
@@ -1,2 +1,4 @@
-SELECT i_number, i_date, i_total, c_id, i_complaint, i_coupon_code, i_coupon_perc
-FROM invoice
+SELECT i_number, i_date, i_total, c_name, if(i_complaint=1,'Igen', 'Nem') AS i_complaint, i_coupon_code, i_coupon_perc
+FROM invoice i, customers c
+WHERE
+i.c_id=c.c_id;
